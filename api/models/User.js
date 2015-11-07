41e1e4c048c187bc9e7b504c88db2a1f290fc75b
Pooler22 <rudypawel12@gmail.com>
2015-11-07 22:01:02 +0100

    user sesion

--- api/models/User.js
+++ api/models/User.js
@@ -1,9 +1,9 @@
 /**
-* User.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
 
 module.exports = {
   schema: true,
@@ -25,12 +25,56 @@ module.exports = {
       type: 'string',
       required: true
     },
-    courses: [
-      {
-        name: 'string',
-        resolved: 'integer'
+    courses: [{
+      name: 'string',
+      resolved: 'integer'
+    }],
+
+    online: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+
+    admin: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+
+    toJSON: function() {
+      var obj = this.toObject();
+      delete obj.password;
+      delete obj.confirmation;
+      delete obj.encryptedPassword;
+      delete obj._csrf;
+      return obj;
+    }
+  },
+  beforeValidation: function(values, next) {
+    if (typeof values.admin !== 'undefined') {
+      if (values.admin === 'unchecked') {
+        values.admin = false;
+      } else if (values.admin[1] === 'on') {
+        values.admin = true;
       }
-    ]
+    }
+    next();
+  },
+
+  beforeCreate: function(values, next) {
+
+    // This checks to make sure the password and password confirmation match before creating record
+    if (!values.password || values.password != values.confirmation) {
+      return next({
+        err: ["Password doesn't match password confirmation."]
+      });
+    }
+
+    require('bcrypt').hash(values.password, 10, function passwordEncrypted(err, encryptedPassword) {
+      if (err) return next(err);
+      values.encryptedPassword = encryptedPassword;
+      // values.online= true;
+      next();
+    });
 
   }
 };
